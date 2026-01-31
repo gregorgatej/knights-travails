befcb64ca3726f09d562c5de5bc62c24d2737e48
Gregor Gatej <gregor.gatej@gmail.com>
2026-01-31 06:34:41 +0100

    feat: add valid_moves method

--- lib/knight_moves.rb
+++ lib/knight_moves.rb
@@ -6,5 +6,14 @@
   ]
 
 def knight_moves(start_pos, end_pos)
+  #TODO
+end
 
+def valid_moves(pos)
+  x, y = pos
+  neighbors = MOVES.map do |dx, dy|
+    [x + dx, y + dy]
+  end
+  neighbors.filter! { |x, y| (0..7).cover?(x) && (0..7).cover?(y) }
+  neighbors
 end
---
+++ main.rb
@@ -0,0 +1,4 @@
+require "./lib/knight_moves"
+
+moves = valid_moves([3, 3])
+p moves
